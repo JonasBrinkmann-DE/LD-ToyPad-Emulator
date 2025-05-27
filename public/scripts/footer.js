3a131424133ff1ef73c00d4a44093d61b070bdbe
TiverT <jonasthebrinkmann@gmail.com>
2025-05-27 21:30:00 +0200

    Saved darkmode state

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -1,3 +1,8 @@
+
+if(Cookies.get("darkmode") == "true")
+{
+  ToggleTheme();
+}
 function OpenCopyright() {
   window.open("https://choosealicense.com/licenses/mit", "about:blank");
 }
@@ -5,17 +10,23 @@ function OpenGithub() {
   window.open("https://github.com/Berny23/LD-ToyPad-Emulator", "about:blank");
 }
 function ToggleTheme() {
-  const $this = document.getElementById("themeSwitcher").children[0];
-  console.log($this);
-  isDarkmode = document.body.classList.contains("dark");
-
-  if (isDarkmode) {
-    document.body.classList.remove("dark");
-    $this.src = "ressources/moon.svg";
-    $this.alt = "Activate lightmode";
-  } else {
-    document.body.classList.add("dark");
-    $this.src = "ressources/sun.svg";
-    $this.alt = "Activate darkmode";
+  const func = function() {
+    const $this = document.getElementById("themeSwitcher").children[0];
+    isDarkmode = document.body.classList.contains("dark");
+    Cookies.set("darkmode", (!isDarkmode));
+    if (isDarkmode) {
+      document.body.classList.remove("dark");
+      $this.src = "ressources/moon.svg";
+      $this.alt = "Activate lightmode";
+    } else {
+      document.body.classList.add("dark");
+      $this.src = "ressources/sun.svg";
+      $this.alt = "Activate darkmode";
+    }
   }
+  if (document.readyState === 'loading'){
+    document.addEventListener("DOMContentLoaded", func);
+  }
+  func();
 }
+
